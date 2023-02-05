f13920639078c9b95251ea027ca63e846a7d8e1e
shikha <zeeshikhasingh19@gmail.com>
2023-02-05 01:20:39 -0800

    ifelse

---
+++ if_else.php
@@ -0,0 +1,31 @@
+<?php
+
+// $x = "100";
+// if($x === "100"){
+
+//     echo "$x is greater";
+
+// }else{
+
+//     echo "$x is lesser";
+
+// } 
+
+
+//this is real life example.
+
+$name = "shikha";
+$gender = "male";
+
+if($gender == "Female"){
+
+    echo "Hello Miss : " . " $name";
+
+}else{
+
+    echo "hello Mrs:" . "$name";
+
+}
+
+
+?>
---
+++ logical_operator.php
@@ -0,0 +1,30 @@
+<?php
+
+// $age =20;
+
+// if($age >= 18 && $age <= 21){
+
+//     echo "your are eligible.";
+// }
+
+//outpute : your are eligible.
+
+$age = 25;
+
+// This is an example of &&.
+
+// if($age >=18 && $age <=21){
+
+//     echo "your are eligible.";
+
+// }
+
+// echo "you  Are Not Eligible.";
+
+
+if(!($age <= 18)){
+
+    echo "You Are Eligible";
+}
+
+?>
